00d0b0efae07620c69825bab96a97120d53f5558
Tanmay Kohale <tanmaykohale0@gmail.com>
2026-08-30 12:54:14 +0530

    revert: restore original MongoDB connection config

--- backend/app/database/mongodb.py
+++ backend/app/database/mongodb.py
@@ -1,6 +1,5 @@
 import os
 
-import certifi
 from dotenv import load_dotenv
 from pymongo import MongoClient
 from pymongo.server_api import ServerApi
@@ -13,7 +12,6 @@ DATABASE_NAME = os.getenv("DATABASE_NAME", "ai_resume_analyzer")
 client = MongoClient(
     MONGODB_URI,
     server_api=ServerApi("1"),
-    tlsCAFile=certifi.where(),
 )
 
 db = client[DATABASE_NAME]
